af58a0ff882b4a47365e62b0d7d197245ecce7c7
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-05-31 22:40:08 +0200

    Finishing up the landing page

--- index.js
+++ index.js
@@ -17,30 +17,4 @@ function submitForm() {
      categories.classList.toggle("show-categories");
  });
 
-// navToggle.addEventListener("click", function () {
-//   menu.classList.toggle("menu");
-// });
-// const navSlide = () => {
-//     const burger = document.querySelector(".hamburger");
-//     const nav = document.querySelector(".header-nav");
-//     const navLinks = document.querySelectorAll(".header-nav a");
-//     //toggle nav
-//     burger.addEventListener("click", () => {
-//         nav.classList.toggle("nav-active");
-
-//     //animate links
-//     navLinks.forEach((link, index) => {
-//         if(link.style.animation) {
-//             link.style.animation = ''
-//         } else {
-//             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
-//     }
-// });
-
-//     //burger animation
-//     burger.classList.toggle("toggle");
-// });
-// }
-
-// navSlide();
 
