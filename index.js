fba4e0bca27611eeeaa0505a9595fef1b47024ee
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-05-31 16:54:25 +0200

    adding navigation and footer

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ function submitForm() {
     document.getElementById("name").value="";
     document.getElementById("email").value="";
 };
+
+
+ const navToggle = document.querySelector(".nav-toggle");
+ const categories = document.querySelector(".nav-categories");
+ navToggle. addEventListener("click", function() {
+     categories.classList.toggle("show-categories");
+ });
+
+// navToggle.addEventListener("click", function () {
+//   menu.classList.toggle("menu");
+// });
+// const navSlide = () => {
+//     const burger = document.querySelector(".hamburger");
+//     const nav = document.querySelector(".header-nav");
+//     const navLinks = document.querySelectorAll(".header-nav a");
+//     //toggle nav
+//     burger.addEventListener("click", () => {
+//         nav.classList.toggle("nav-active");
+
+//     //animate links
+//     navLinks.forEach((link, index) => {
+//         if(link.style.animation) {
+//             link.style.animation = ''
+//         } else {
+//             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+//     }
+// });
+
+//     //burger animation
+//     burger.classList.toggle("toggle");
+// });
+// }
+
+// navSlide();
+
