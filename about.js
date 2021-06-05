bd9b49e05c01cd92dd13d1c56cb7c6493ba08f23
Agata Figura <agata.figura1@gmail.com>
2021-06-05 21:09:45 +0200

    FINAL VERSION about&guide

--- about.js
+++ about.js
@@ -7,3 +7,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const navToggle = document.querySelector(".nav-toggle");
+const categories = document.querySelector(".nav-categories");
+navToggle.addEventListener("click", function () {
+  categories.classList.toggle("show-categories");
+});
